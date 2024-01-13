4211ae361b69e1069e9859f12a51dbef9aa2cc5d
Fellarrusto <filippo.leonelli@linksmt.it>
2024-01-14 00:10:26 +0100

    Added https support

--- server.py
+++ server.py
@@ -25,4 +25,4 @@ def new_message():
     return "Received", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, ssl_context=("certificate.crt", "private.key"))
